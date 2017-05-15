59d24ac3a392772cbdb05f4f47f425a741cd7f77
Will Boyce <me@willboyce.com>
2017-05-15 22:01:11 +0100

    parse all files matching glob `/var/run/openvpn/server-*.status`

--- libnss_openvpn.c
+++ libnss_openvpn.c
@@ -1,4 +1,4 @@
-/*  
+/*
  *  Copyright © 2012-2014 Gonéri Le Bouder
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -20,7 +20,7 @@
  *    nss-gw-name, © 2010 Joachim Breitner.
  */
 
-#define OPENVPN_STATUS_FILE "/var/run/openvpn.server.status"
+#define OPENVPN_STATUS_FILE_GLOB "/var/run/openvpn/server-*.status"
 #include <stdio.h>
 #include <string.h>
 #include <nss.h>
@@ -28,6 +28,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <glob.h>
 
 #define BUFLEN 256
 
@@ -46,106 +47,107 @@ enum nss_status _nss_openvpn_gethostbyname_r (
         int *errnop,
         int *h_errnop) {
 
-
-    FILE * fh = fopen(OPENVPN_STATUS_FILE, "r");
-
-    char * t;
-    char * current_ip;
-    char * current_hostname;
-    size_t idx, astart;
-
-    char strbuf[BUFLEN];
-
-    if (!fh || strcmp(name+strlen(name)-4, ".vpn") != 0) {
-        if (fh) {
-            fclose(fh);
-        }
+    if (strcmp(name+strlen(name)-4, ".vpn") != 0) {
         *errnop = EINVAL;
         *h_errnop = NO_RECOVERY;
         return NSS_STATUS_UNAVAIL;
     }
 
-    int in = 0;
-    while (fgets(strbuf, BUFLEN, fh)) {
+    glob_t status_files;
+    if (glob(OPENVPN_STATUS_FILE_GLOB, 0, NULL, &status_files) == 0) {
+        int i = 0;
+        for (i = 0; i < status_files.gl_pathc; i++) {
+            FILE * fh = fopen(status_files.gl_pathv[i], "r");
 
-        if (strncmp(strbuf, "Virtual Address", 10) == 0) {
-            in = 1;
-            continue;
+            char * t;
+            char * current_ip;
+            char * current_hostname;
+            size_t idx, astart;
+
+            char strbuf[BUFLEN];
+
+            if (!fh) {
+                continue;
+            }
+
+            int in = 0;
+            while (fgets(strbuf, BUFLEN, fh)) {
+                if (strncmp(strbuf, "Virtual Address", 10) == 0) {
+                    in = 1;
+                    continue;
+                }
+                if (strncmp(strbuf, "GLOBAL STATS", 10) == 0) {
+                    in = 0;
+                    continue;
+                }
+                if (!in) {
+                    continue;
+                }
+
+                current_ip=strbuf;
+                t = strchr(current_ip, ',');
+                if (t == NULL) {
+                    continue;
+                }
+                *t = 0;
+
+                current_hostname=current_ip+strlen(current_ip)+1;
+                t = strchr(current_hostname, ',');
+                if (t == NULL) {
+                    continue;
+                }
+                *t = 0;
+
+                if (strlen(current_hostname) + 5 > BUFLEN) {
+                    continue;
+                }
+                strcpy(t, ".vpn\0");
+
+                // Is it the host I'm looking for?
+                if (strcmp(current_hostname, name)!=0) {
+                    continue;
+                }
+
+                *((char**) buffer) = NULL;
+                result->h_aliases = (char**) buffer;
+                idx = sizeof(char*);
+
+                /* Official name */
+                strcpy(buffer+idx, name);
+                result->h_name = buffer+idx;
+                idx += strlen(name)+1;
+                ALIGN(idx);
+
+                result->h_addrtype = AF_INET;
+                result->h_length = sizeof(uint32_t);
+
+                struct in_addr addr;
+
+                inet_pton(AF_INET, current_ip, &addr);
+
+                char a[2000];
+                inet_ntop(AF_INET, &addr.s_addr, a, 2000);
+                //fprintf(stdout, "IP found: %s\n",a);
+
+                astart = idx;
+                memcpy(buffer+astart, &addr.s_addr, sizeof(uint32_t));
+                idx += sizeof(uint32_t);
+
+                result->h_addr_list = (char**)(buffer + idx);
+                result->h_addr_list[0] = buffer + astart;
+                result->h_addr_list[1] = NULL;
+
+                fclose(fh);
+                globfree(&status_files);
+                return NSS_STATUS_SUCCESS;
+            }
         }
-        if (strncmp(strbuf, "GLOBAL STATS", 10) == 0) {
-            in = 0;
-            continue;
-        }
-
-        if (!in) {
-            continue;
-        }
-
-        current_ip=strbuf;
-        t = strchr(current_ip, ',');
-        if (t == NULL) {
-            continue;
-        }
-        *t = 0;
-
-        current_hostname=current_ip+strlen(current_ip)+1;
-        t = strchr(current_hostname, ',');
-        if (t == NULL) {
-            continue;
-        }
-        *t = 0;
-
-        if (strlen(current_hostname) + 5 > BUFLEN) {
-            continue;
-        }
-        strcpy(t, ".vpn\0");
-
-        // Is it the host I'm looking for?
-        if (strcmp(current_hostname, name)!=0) {
-            continue;
-        }
-
-        *((char**) buffer) = NULL;
-        result->h_aliases = (char**) buffer;
-        idx = sizeof(char*);
-
-        /* Official name */
-        strcpy(buffer+idx, name);
-        result->h_name = buffer+idx;
-        idx += strlen(name)+1;
-        ALIGN(idx);
-
-
-        result->h_addrtype = AF_INET;
-        result->h_length = sizeof(uint32_t);
-
-        struct in_addr addr;
-
-        inet_pton(AF_INET, current_ip, &addr);
-
-        char a[2000];
-        inet_ntop(AF_INET, &addr.s_addr, a, 2000);
-        //fprintf(stdout, "IP found: %s\n",a);
-
-        astart = idx;
-        memcpy(buffer+astart, &addr.s_addr, sizeof(uint32_t));
-        idx += sizeof(uint32_t);
-
-        result->h_addr_list = (char**)(buffer + idx);
-        result->h_addr_list[0] = buffer + astart;
-        result->h_addr_list[1] = NULL;
-
-        fclose(fh);
-
-        return NSS_STATUS_SUCCESS;
+        globfree(&status_files);
     }
-
-    fclose(fh);
 
     *errnop = EINVAL;
     *h_errnop = NO_RECOVERY;
     return NSS_STATUS_UNAVAIL;
-
 }
 
 enum nss_status _nss_openvpn_gethostbyname2_r(
